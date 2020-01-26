17e7626fc15731d0d1a94acaccd812ee0898b4f6
Baldemar Sepulveda <balde.sepu@pm.me>
2020-01-25 18:06:49 -0600

    Added routing for root app-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ThreadComponent } from './thread/thread.component';
+import { ThreadDetailComponent } from './thread-detail/thread-detail.component';
+import { CommunityComponent } from './community/community.component';
+import { CommunityDetailComponent } from './community-detail/community-detail.component';
+import { CommentComponent } from './comment/comment.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login", component:LoginComponent },
+  {path:"register", component:RegisterComponent },
+  {path:"thread", component:ThreadComponent },
+  {path:"thread-detail", component:ThreadDetailComponent },
+  {path:"community", component:CommunityComponent },
+  {path:"community-detail", component:CommunityDetailComponent },
+  {path:"comment", component:CommentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CommentComponent } from './comment/comment.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommunityComponent } from './community/community.component';
@@ -9,6 +10,7 @@ import { RegisterComponent } from './register/register.component';
 import { CommunityDetailComponent } from './community-detail/community-detail.component';
 import { ThreadComponent } from './thread/thread.component';
 import { ThreadDetailComponent } from './thread-detail/thread-detail.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { ThreadDetailComponent } from './thread-detail/thread-detail.component';
     RegisterComponent,
     CommunityDetailComponent,
     ThreadComponent,
-    ThreadDetailComponent
+    ThreadDetailComponent,
+    CommentComponent,
   ],
   imports: [
     BrowserModule,
