992d06de1ff526655715c2774984ebf791ca5d79
Baldemar Sepulveda <balde.sepu@pm.me>
2020-01-26 13:01:15 -0600

    Added binding of data for thread will replace classes later

---
+++ src/app/thread/classes/thread.class.ts
@@ -0,0 +1,7 @@
+import { User } from './user.class';
+
+export class Thread{
+    title:string;
+    description:string;
+    user:User;
+}
--- src/app/thread/thread.component.ts
+++ src/app/thread/thread.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Thread } from './classes/thread.class';
+import { User } from './classes/user.class';
 
 @Component({
   selector: 'app-thread',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thread.component.css']
 })
 export class ThreadComponent implements OnInit {
-
-  constructor() { }
+  private thread:Thread;
+  constructor() {
+    this.thread = new Thread();
+    // This is just for testing remove when you have a service.
+    this.thread.title = "Look at this game!!!";
+    this.thread.description = "So I just bought this game and I think its really cool!!!";
+    this.thread.user = new User();
+    this.thread.user.username = "someuser";
+  }
 
   ngOnInit() {
   }
