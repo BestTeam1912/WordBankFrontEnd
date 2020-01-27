278d1d186cd2e0d86c4e8b55b22654cf018b4698
thitalow <51796772+thitalow@users.noreply.github.com>
2020-01-27 16:43:56 -0600

    p2 thita: community mock data and added forms module (#8)

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { COMMUNITIES } from '../mock-communities';
+import { Community } from './classes/community.class';
+
 
 @Component({
   selector: 'app-community',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./community.component.css']
 })
 export class CommunityComponent implements OnInit {
+  communities = COMMUNITIES;
+  selectedCom: Community;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(com: Community): void {
+    this.selectedCom = com;
   }
 
 }
