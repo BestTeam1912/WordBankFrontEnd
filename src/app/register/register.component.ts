c53401a11de2dd330f0eab9f951fa19e35a9009e
John-Paul Vargas <jp.vargas@live.com>
2020-01-31 08:26:51 -0600

    error when trying to use twoway binding to username, still debugging

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { User } from "../user";
 
 @Component({
   selector: 'app-register',
@@ -8,7 +9,33 @@ import { UserService } from '../user.service';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  private user:User;
+  private status:boolean
+  constructor(private service:UserService) {
+    this.user=new User();
+   }
+  
+   registerUser(){
+     console.log(this.user.username);
+     console.log(this.user.password);
+     this.service.registerUser(this.user).subscribe(res=>status);
+     if(status){
+       window.alert("User was registered successfully");
+     }else{
+       window.alert("There was an error, please try again");
+     }
+   }
+
+   registerAdmin(){
+    console.log(this.user.username);
+    console.log(this.user.password);
+    this.service.registerAdmin(this.user).subscribe(res=>status);
+    if(status){
+      window.alert("User was registered successfully");
+    }else{
+      window.alert("There was an error, please try again");
+    }
+  }
 
   ngOnInit() {
   }
