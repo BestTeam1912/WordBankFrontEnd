12f1fa5c2e242f022060ebe03dd2a5fcbb84061a
JP204 <jp.vargas@live.com>
2020-02-01 17:29:09 -0600

    sessions kind done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,25 +8,27 @@ import { User } from '../user';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
-  private user:User;
-  private cuser:User;
-  constructor(private service:UserService) {
-    this.user=new User();
-   }
 
-   login(){
-    console.log(this.user.username);
-    console.log(this.user.password);
-     this.service.login(this.user).subscribe(res=>{
-       this.cuser = res;
-       console.log(this.cuser.username);
-     })
-     if(this.cuser.username === "login confirmed"){
-      window.alert("you have logged in successfully");
-    }else if(this.cuser.username === "no match" || this.cuser.username === "user not found"){
-      window.alert("Your credentials are incorrect, please try again");
-    }
+  private user: User;
+  private cuser: User;
+  constructor(private service: UserService) {
+    this.user = new User();
+  }
+
+  login() {
+    this.service.login(this.user).subscribe(res => {
+      this.cuser = res;
+      if (this.cuser != null) {
+        window.alert("you have logged in successfully");
+      } else {
+        window.alert("Your credentials are incorrect, please try again");
+      }
+      console.log(this.cuser.type);
+      this.cuser.password="";
+      sessionStorage.setItem("user", JSON.stringify(this.cuser));
+      console.log(sessionStorage.getItem("id"));
+    })
+    
   }
 
   ngOnInit() {
