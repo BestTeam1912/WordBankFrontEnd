e58ae2667fe3d6c54f2af520af0e9e650dfc1f8b
Cody Harkins <us3rs1@gmail.com>
2020-02-02 17:08:43 -0600

    small fix to app component spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ fdescribe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Relpy app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Relpy');
   });
 });
