73c73d7d13b8f6367a06ae69277c5f0b6fd0a06c
Cody Harkins <us3rs1@gmail.com>
2020-02-01 23:07:32 -0600

    title -> Relpy & chrome launcher to karma.conf.ts

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+	browsers: ['Chrome'],
+	customLaunchers:{
+		base: 'ChromeHeadless',
+		flags: ['--no-sandbox']
+	},
     singleRun: false,
     restartOnFileChange: true
   });
